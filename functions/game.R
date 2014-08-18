5d98a97036f0ed3e0951a0ce3ff8fc454c4a6b05
leerichardson <leerichardson2013@gmail.com>
2014-08-18 12:51:50 -0400

    commiting an update to load onto my new laptop!

--- functions/game.R
+++ functions/game.R
@@ -1,3 +1,42 @@
+game <- function(team_one, team_two, year_one, year_two) {
+  
+  #Read in all of the functions that will be needed for the game
+  #source("functions/teams.R")
+  source("functions/determine_event.R")
+  source("functions/determine_player.R")
+  source("functions/free_throw.R")
+  source("functions/two_point_shot.R")
+  source("functions/three_point_shot.R")
+  source("functions/rebound.R")
+  source("functions/determine_rebounder.R")
+  
+  #Using inputs, read in the players who will be on each team
+  team1 <- subset(merged_data, Tm == team_one & year == year_one & MP >= 500)
+  team2 <- subset(merged_data, Tm == team_two & year == year_two & MP >= 500)
+  
+  #Based on the number of players on each team, count how many players will be in the box score
+  num_rows <- nrow(team1) + nrow(team2)
+  print(c(team1$Player, team2$Player))
+  
+  #Initialize the Box Score to all 0's
+  box_score <- as.data.frame(matrix(0, nrow=num_rows, ncol=10))
+  
+#   #Name the data frame that will be collecting all of the results
+#   colnames(box_score) <- c("team1_1", "team1_2", "team1_3", "team1_4", "team1_5", 
+#                            "team2_1", "team2_2", "team2_3", "team2_4", "team2_5")
+#   rownames(box_score) <- c("2M", "2A", "3M", "3A", "FTM", "FTA", "TO", "OREB", "DREB")  
+#   totals_min <- sum(team1$MPG)
+#   team1$possession_probability <- team1$MPG/totals_min
+#   possession_five_team1 <- team1[sample(nrow(team1), size=5, prob=team1$possession_probability),]
+    
+    
+}
+
+#Testing the game
+game("LAL", "MIA", 2013, 2013)
+
+
+
 #Read in all of the functions that will be needed for the game
 source("functions/teams.R")
 source("functions/two_point_shot.R")
@@ -6,7 +45,6 @@ source("functions/determine_player.R")
 source("functions/free_throw.R")
 source("functions/rebound.R")
 source("functions/three_point_shot.R")
-source("functions/possession.R")
 source("functions/determine_rebounder.R")
 
 #Read in the input data which sets the parameters for the game 
@@ -50,6 +88,10 @@ for(i in 1:num_pos) {
   #Determine which Player will use the possession based on Individual Usage Rates  
   determine_player(team2[1,"usage"], team2[2,"usage"], team2[3,"usage"], team2[4,"usage"], team2[5,"usage"])
 }
+
+
+
+
 
 
 
--- functions/teams.r
+++ functions/teams.r
@@ -4,80 +4,65 @@ require(plyr)
 #Read in the Dataset with all of the individual Player Data
 all_seasons <- read.csv("data/all_data.csv", header=TRUE)
 season_2013 <- read.csv("data/2013_season.csv", header=TRUE)
+rpm_data <- read.csv("data/RPM_data.csv", header=TRUE)
+
+#Merge the rpm data onto all of the Player Data
+merged_data <- merge(all_seasons, rpm_data, by = c("Player", "year"), all.x = TRUE)
 
 #Create the Minutes Per Game Variable
-all_seasons$MPG <- all_seasons$MP/all_seasons$G
+merged_data$MPG <- merged_data$MP/merged_data$G
 
 #Create individual Player Offensive distributions
-all_seasons$total_pos <- all_seasons$X3PA + all_seasons$X2PA + .44*all_seasons$FTA + all_seasons$TOV
-all_seasons$three_dist <- all_seasons$X3PA/all_seasons$total_pos
-all_seasons$two_dist <- all_seasons$X2PA/all_seasons$total_pos
-all_seasons$ft_dist <- .44*all_seasons$FTA/all_seasons$total_pos
-all_seasons$tov_dist <- all_seasons$TOV/all_seasons$total_pos
-all_seasons <- rename(all_seasons, replace=c("X3P."= "three_perc",
+merged_data$total_pos <- merged_data$X3PA + merged_data$X2PA + .44*merged_data$FTA + merged_data$TOV
+merged_data$three_dist <- merged_data$X3PA/merged_data$total_pos
+merged_data$two_dist <- merged_data$X2PA/merged_data$total_pos
+merged_data$ft_dist <- .44*merged_data$FTA/merged_data$total_pos
+merged_data$tov_dist <- merged_data$TOV/merged_data$total_pos
+merged_data <- rename(merged_data, replace=c("X3P."= "three_perc",
                                              "X2P."= "two_perc","FT."= "ft_perc",
                                              "DRB."="indv_drb","ORB."= "indv_orb", "USG."="usage"))
 
-#Scale the Oreb, Dreb, and Usage rate percentages to one
-#season_2013[,"indv_orb"] = season_2013[,"indv_orb"]/100
-#season_2013[,"indv_drb"] = season_2013[,"indv_drb"]/100
-#season_2013[,"usage"] = season_2013[,"usage"]/100
-
-#Create individual Player Offensive distributions
-#total_pos <- X3PA + X2PA + .44*FTA + TOV
-#three_dist <- X3PA/total_pos
-#two_dist <- X2PA/total_pos
-#ft_dist <- .44*FTA/total_pos
-#tov_dist <- TOV/total_pos
-#season_2013 <- cbind(season_2013, total_pos, three_dist, two_dist, ft_dist, tov_dist)
-#season_2013 <- rename(season_2013, replace=c("X3P."= "three_perc",
-#                                             "X2P."= "two_perc","FT."= "ft_perc",
-#                                             "DRB."="indv_drb","ORB."= "indv_orb", "USG."="usage"))
-
-#Scale the Oreb, Dreb, and Usage rate Percentages by 100 
-#season_2013[,"indv_orb"] = season_2013[,"indv_orb"]/100
-#season_2013[,"indv_drb"] = season_2013[,"indv_drb"]/100
-#season_2013[,"usage"] = season_2013[,"usage"]/100
-
 #Grab the names of the variables to scale, and then scale them by 100
-df <- all_seasons[,c("indv_orb", "indv_drb", "TRB.", "AST.", "STL.", "BLK.", "TOV.", "usage")]
+df <- merged_data[,c("indv_orb", "indv_drb", "TRB.", "AST.", "STL.", "BLK.", "TOV.", "usage")]
 for(i in names(df)){
-  all_seasons[[i]] <- all_seasons[[i]]/100
+  merged_data[[i]] <- merged_data[[i]]/100
 }
+
+#Set up the Dataset where all Game's will pull from
+write.csv(merged_data, file = "data/input_data.csv")
 
 #Create a team that represents the 2014 Lakers and 2014 Heat
 team1 <- "LAL"
 team2 <- "MIA"
 year_of_game <- 2013
 
-team1 <- subset(all_seasons, Tm == team1 & year == year_of_game & MP >= 500)
+team1 <- subset(merged_data, Tm == team1 & year == year_of_game & MP >= 500)
 totals_min <- sum(team1$MPG)
 team1$possession_probability <- team1$MPG/totals_min
 possession_five_team1 <- team1[sample(nrow(team1), size=5, prob=team1$possession_probability),]
 
-team2 <- subset(all_seasons, Tm == team2 & year == year_of_game & MP >= 500)
+team2 <- subset(merged_data, Tm == team2 & year == year_of_game & MP >= 500)
 totals_min <- sum(team2$MPG)
 team2$possession_probability <- team2$MPG/totals_min
 possession_five_team2 <- team2[sample(nrow(team2), size=5, prob=team2$possession_probability),]
 
-
 #Create a dataset of just the individual teams and order the players by minutes played
-#lakers <- subset(season_2013, Tm == "LAL")
-#lakers_ordered <- lakers[order(lakers[,"MP"], decreasing=TRUE),]
-#heat <- subset(season_2013, Tm == "MIA")
-#heat_ordered <- heat[order(heat[,"MP"], decreasing=TRUE),]
-#rm(lakers, heat)
+# lakers <- subset(season_2013, Tm == "LAL")
+# lakers_ordered <- lakers[order(lakers[,"MP"], decreasing=TRUE),]
+# heat <- subset(season_2013, Tm == "MIA")
+# heat_ordered <- heat[order(heat[,"MP"], decreasing=TRUE),]
+# rm(lakers, heat)
+# 
+# Sort the teams by the top 5 players
+# lakers_five <- lakers_ordered[1:5,]
+# heat_five <- heat_ordered[1:5,]
 
-#Sort the teams by the top 5 players
-#lakers_five <- lakers_ordered[1:5,]
-#heat_five <- heat_ordered[1:5,]
-
-#Keep only the inputs that will matter in the game 
-#lakers_input <- lakers_five[,c("Player", "three_perc", "two_perc", "ft_perc", 
-                 #              "usage", "indv_drb", "indv_orb", "three_dist", 
-                 #               "two_dist", "ft_dist", "tov_dist")]
-#heat_input <- heat_five[,c("Player", "three_perc", "two_perc", "ft_perc", 
-                 #          "usage", "indv_drb", "indv_orb", "three_dist", 
-                 #           "two_dist", "ft_dist", "tov_dist")]
+# #Keep only the inputs that will matter in the game 
+# lakers_input <- possession_five_team1[,c("Player", "three_perc", "two_perc", "ft_perc", 
+#                               "usage", "indv_drb", "indv_orb", "three_dist", 
+#                                 "two_dist", "ft_dist", "tov_dist")]
+# heat_input <- possession_five_team2[,c("Player", "three_perc", "two_perc", "ft_perc", 
+#                            "usage", "indv_drb", "indv_orb", "three_dist", 
+#                             "two_dist", "ft_dist", "tov_dist")]
 
 
