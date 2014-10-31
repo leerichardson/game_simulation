731be51461f2b5f4a5066fca71c70d4872343568
leerichardson <leerichardson2013@gmail.com>
2014-10-31 12:20:56 -0400

    added a xtest dataset and todo list

--- scripts/detail_player_join.R
+++ scripts/detail_player_join.R
@@ -13,15 +13,24 @@ con <- dbConnect(drv="SQLite", dbname="nba.db")
 # Inspect RPM and ESPN player's in 2009
 player_rpm_merge <- dbGetQuery(con,'SELECT * FROM players LEFT JOIN rpm ON 
                                players.playerName=rpm.Player AND players.SEASON=rpm.year_
-                               AND players.team = rpm.Tm WHERE players.SEASON = 2009')
+                               AND players.team = rpm.Tm')
 
-espn_2009 <- dbGetQuery(con, 'SELECT * FROM players WHERE SEASON = 2009')
-rpm_2009 <- dbGetQuery(con, 'SELECT * FROM rpm WHERE year_ = 2009 AND Player LIKE "%Alston%"')
-full_cases <- filter(player_rpm_merge, complete.cases(player_rpm_merge))
+#Pulling the relevant columns 
+player_inputs <- select(player_rpm_merge, playerId, playerName, SEASON, Tm, MPG, PER)
+filter(player_inputs, !complete.cases(player_inputs))
 
-############## RPM AND ESPN ARE OFF BY ONE YEAR ################################
+######### CONSTRUCT THE X TEST DATASET ###############
 
-full_game_summary <- dbGetQuery(con, 'SELECT * FROM gameScore LEFT JOIN gameDetail 
+full_game_summary <- dbGetQuery(con, 'SELECT gameScore.match_id, gameScore.gameDate, gameScore.game_year, 
+gameScore.home_team, gameScore.home_team_score, gameScore.visit_team, gameScore.visit_team_score, 
+home, gameDetail.playerID, players.playerName FROM gameScore LEFT JOIN gameDetail 
                                 ON gameScore.match_id = gameDetail.match_id LEFT JOIN
                                 players ON gameDetail.playerId = players.playerId
-                                WHERE gameScore.game_year = players.SEASON')
+                                WHERE gameScore.game_year = players.SEASON 
+                                AND gameDetail.netPoints is not null')
+
+#### TEST PULLING DATA FROM PREVIOUS YEAR ###########
+game_train <- dbGetQuery(con, 'SELECT * FROM gameScore LEFT JOIN gameDetail 
+    ON gameScore.match_id = gameDetail.match_id LEFT JOIN players ON gameDetail.playerId = players.playerId
+    WHERE gameDetail.match_id = 311225018 AND players.SEASON = ')
+
