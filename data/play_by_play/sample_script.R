15d788c7f9de115cf6db84b9e15aa1159535444f
leerichardson <leerichardson2013@gmail.com>
2014-12-07 23:47:39 -0500

    First draft of the final paper (comments for the conclusion

--- data/play_by_play/sample_script.R
+++ data/play_by_play/sample_script.R
@@ -1,11 +1,19 @@
-##################################################################
-## Download all of the raw game play by play files from NBA.com ##
-##################################################################
+####################################################################
+## Download all of the raw game play by play files from NBA.com ####
+####################################################################
 library(XML)
+library(RCurl)
 
-first_date <- "http://www.nba.com/gameline/20091001/"
+## Read in the first date
+first_date <- "http://www.nba.com/gameline/20091027/"
 
+## Extract all Game links from this home link
+doc <- htmlParse(first_date, isHTML=TRUE)
+game_links <- unique(getHTMLLinks(doc))
+game_urls <- paste("http://www.nba.com/", game_links[grep('gameinfo', game_links)], sep="")
 
-nba_one <- "http://www.nba.com/games/20101201/CHANOH/gameinfo.html"
-sample_game <- readLines(nba_one)
-doc <- htmlParse(sample_game)
+## Loop through each game to get the play by play data
+for(g in game_urls){
+ print(g) 
+}
+
